84e03824785933a1103e4b715814fee28820264a
Ruchita-gif <ruchitayadavannavar0@gmail.com>
2025-04-08 22:16:19 +0530

    Added HTML pages for hospital website

---
+++ open_all_html.bat
@@ -0,0 +1,7 @@
+@echo off
+start "" "d:\Nithin Hospital\index.html"
+start "" "d:\Nithin Hospital\Se.html"
+start "" "d:\Nithin Hospital\image.html"
+start "" "d:\Nithin Hospital\av.html"
+start "" "d:\Nithin Hospital\br.html"
+start "" "d:\Nithin Hospital\conta.html"
